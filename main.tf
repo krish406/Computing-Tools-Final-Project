040fcd33d8523f2545d941c12b259582ea53c1f7
krish406 <155498135+krish406@users.noreply.github.com>
2026-07-21 15:46:46 -0400

    created terraform file, set provider as env var

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 4.2.0"
+    }
+  }
+}
+
+provider "docker" {
+
+}
+
+resource "docker_image" "flask-project-app" {
+  name = "flask-project:v1"
+  
+  build {
+    context    = "." 
+    dockerfile = "Dockerfile"
+  }
+}
+
+# Run the container using the built image
+resource "docker_container" "flask-project-container" {
+  name  = "running-flask-project-v1"
+  image = docker_image.flask-project-app.image_id
+  
+  ports {
+    internal = 8080
+    external = 8080
+  }
+}
+
