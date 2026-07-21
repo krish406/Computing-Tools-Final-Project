6dd302044080cf68d7e404a92e84713f79bab160
krish406 <155498135+krish406@users.noreply.github.com>
2026-07-21 18:21:02 -0400

    refactor: fixed terraform

--- main.tf
+++ main.tf
@@ -15,15 +15,6 @@ data "docker_image" "flask-project-app" {
   name = "flask-project:v1"
 }
 
-resource "docker_container" "flask-project-container" {
-  name  = "running-flask-project-v1"
-  image = data.docker_image.flask-project-app.image_id
-  ports {
-    internal = 8080
-    external = 8080
-  }
-}
-
 # Run the container using the built image
 resource "docker_container" "flask-project-container" {
   name  = "running-flask-project-v1"
