1c3e16ec0cd24f697101d8133011a794874946bd
krish406 <155498135+krish406@users.noreply.github.com>
2026-07-21 11:21:23 -0400

    feat: added port configuration for application

--- app/hello.py
+++ app/hello.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 8080))
+    )
